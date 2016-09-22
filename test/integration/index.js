a28e43cc665b1eec2cc8672de26668f4d0cb7fb3
Simmons Pang <simmons.pang@pj.ca>
2016-09-22 15:34:17 -0400

    initial commit

---
+++ test/integration/index.js
@@ -0,0 +1,7 @@
+var assert = require('chai').assert;
+
+describe('integration test', function() {
+    it('should pass', function() {
+        assert.isNull(null);
+    });
+});
---
+++ test/unit/index.js
@@ -0,0 +1,7 @@
+var assert = require('chai').assert;
+
+describe('unit test', function() {
+    it('should pass', function() {
+        assert.isNull(null);
+    });
+});
